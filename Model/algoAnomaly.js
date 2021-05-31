1be153c0a48da0c40bbc8c6eac5dec0cd3f4b603
yosi058 <yosefnatanb@gmail.com>
2021-05-31 16:02:23 +0300

    upload a server

---
+++ Model/algoAnomaly.js
@@ -0,0 +1,208 @@
+const AnomalyImport = require('./anomalydetection')
+const enclosingCircle = require('smallest-enclosing-circle')
+var anomaly = new AnomalyImport.Anomaly();
+// having the id and the cf - learn normal.
+var collection_train = {}; 
+//for the model data like id, upload time and status position 
+var MODELS = {};
+
+//The class is responsible for detecting the anomalies 
+class AlgoAnomaly{
+
+    /*
+    The constructor receives a dictionary of the exception name and data,
+     a Boolean flag of the detector and the ID of the detector for
+      future support in keeping all detectors anomalies in the data structure
+    */
+    constructor(dictionary, isHybrid,model_id){
+        this.dictionary = dictionary;
+        this.isHybrid = isHybrid;
+        this.model_id  = model_id;
+        //Array with data of each pair of correlations
+        this.cfArr=[]  
+    }
+    //The method reveals correlations and then the treshold
+    learnNormal(){
+        this.pearson()
+        this.treshold_points()
+    }
+
+    //The method reveals correlations
+    pearson(){
+        let i=0, j=0;
+        //Search for correlations
+        for( const key1 in this.dictionary){
+            i++;
+            j=0;
+            let max=0;
+            //The variable holds the name of the column with the highest correlation
+            let maxFeature = null;
+            for( const key2 in this.dictionary){
+                j++;
+                //Are these columns we have already tested
+                if(i>=j){
+                    continue
+                }
+                //The correlation
+                let num =0;
+                num= Math.abs(anomaly.pearson(this.dictionary[key1], this.dictionary[key2],this.dictionary[key2].length ));
+                //save the max corelation
+                if(num>max){
+                    max = num;
+                    maxFeature =  key2;
+                }
+
+            }
+           //Saving the data
+             if(this.isHybrid){
+                if(max>=0.5){
+                    this.cfArr.push({feature1: key1, feature2: maxFeature, cor: max});
+                }
+                }else{
+                    if(max>=0.9){
+                        this.cfArr.push({feature1: key1, feature2: maxFeature, cor: max});
+
+                    }
+             }
+        }
+    }
+
+    treshold_points(){
+        //Go over all the cf
+        for(let j=0; j<this.cfArr.length;j++ ){
+            let arrPoints=[];
+            let x=0,  y=0, size=0;
+            let cf = this.cfArr[j];
+            size = this.dictionary[cf.feature1].length;
+            //Create an array of points from the correlative columns 
+            for(let i =0;i<size;i++){
+                 x = this.dictionary[cf.feature1][i];
+                 y = this.dictionary[cf.feature2][i];
+               let point = new AnomalyImport.Point(parseFloat(x),parseFloat(y));
+               arrPoints.push(point);
+            }
+            //for line
+            if(cf.cor>=0.9){
+            //make line
+             let l = anomaly.linear_reg(arrPoints,arrPoints.length);
+             //save the line un the cf
+             this.cfArr[j].line_reg = l;
+           
+             //find treshold
+            let max_line = 0;
+            for (let i =0;i<arrPoints.length;i++){
+               let dev_line = anomaly.dev(arrPoints[i],l);
+               if(dev_line>max_line){
+                   max_line=dev_line;
+               }
+            }
+            this.cfArr[j].treshold = max_line * 1.1;
+       
+            // if treshold < 0.9 (circle case)
+            }else{
+                let circle = enclosingCircle(arrPoints);
+                this.cfArr[j].circle = circle;
+                let radius =  circle.r;
+               let max_circle = 0;
+                for (let i =0;i < arrPoints.length;i++){
+                    let dev_circle = anomaly.distance(arrPoints[i],circle.x,circle.y);
+                    if(dev_circle>max_circle){
+                        max_circle=dev_circle;
+                    }
+                 }
+                this.cfArr[j].treshold = max_circle * 1.1;
+                this.cfArr[j].radius = radius;
+                //update the global data 
+                }
+                
+        }  
+        //for future support in keeping all detectors anomalies in the data structur
+        this.updateData();
+    }
+//for future support in keeping all detectors anomalies in the data structur
+    updateData(){
+        collection_train[this.model_id] = this.cfArr;
+        MODELS[this.model_id].status = "ready";
+    }
+    
+}
+
+//Global variables
+var start=-1, end=0;
+var arrSpan=[];
+
+//The method reveals anomalies
+function detect(dictionary, id){
+    
+    let cfArr = collection_train[id];
+   
+    let anomalies= new Map();
+    let size = cfArr.length;
+    
+    for (let i =0; i<size;i++){
+        let cf = cfArr[i];
+        //The columns
+        if(!(dictionary[cf.feature1]) || !(dictionary[cf.feature2] )){
+           continue;
+        }
+       
+        let feature1= dictionary[cf.feature1];
+        let feature2 = dictionary[cf.feature2];
+        
+        //Go over  the columns
+        for(let j =0; j<feature1.length;j++){
+            //let point = new AnomalyImport.Point(parseFloat(feature1[j]), parseFloat(feature2[j]));
+            let point = new AnomalyImport.Point(feature1[j], feature2[j]);
+
+             //case of line reg (treshold >=0.9)
+            if(cf.cor>= 0.9 ){  
+             let num = anomaly.dev(point,cf.line_reg);
+            //there is anomaly 
+             if(num > cf.treshold){
+                 //save the anomaly
+                saveAnomaly(j); 
+            }
+               //case of circle (treshold < 0.9)
+            } else{
+                let num = anomaly.distance(point,cf.circle.x,cf.circle.y);
+                 //there is anomaly 
+                if(num > cf.radius){
+                  saveAnomaly(j); 
+                }
+            }
+
+            }
+            //If there were no anomalies
+            if(start==-1){
+                //save
+                anomalies.set(cf.feature1 +"  <-->  " + cf.feature2, [])
+
+            }else{
+                //save
+                arrSpan.push([start,end+1]);
+                anomalies.set(cf.feature1 +"  <-->  " + cf.feature2, arrSpan)
+
+            }
+            //Reset for next loop
+            start=-1;
+            arrSpan=[];
+        }
+        return anomalies;
+    }
+
+    //The function saves the anomalies by ranges
+    function saveAnomaly(index){
+        if(start === -1){
+            start =index;
+            end=index;
+        }else if(index=== end+1){
+            end++;
+        }else{
+            arrSpan.push([start,end+1]);
+            start= index;
+            end = index;
+        }
+    }
+
+    //import
+module.exports = {AlgoAnomaly,collection_train,MODELS,detect};
---
+++ README.md
@@ -0,0 +1,69 @@
+# webApp
+Link to github-https://github.com/avivdimri/webApp. \
+Link for the presentation of Web App- \
+
+A Web app server that implements a REST-API for the clients,and a single web-page.\
+The server supports parallelism requests. which given a flight data - find irregular behavior at the flight and report it to the client.\
+There is two possible ways to connect to the server -\
+1. By the client web page appliction - the client just need to upload the files and the app will connect to the server by itself.\
+2. By himself - the client could connect dircet to the server and ask for the answers.
+
+## Collaborators
+This program was developed by four student, Ori Choen, Ori Abramnovich, Aviv Dimri and Yosef Berebi, CS students from Bar-Ilan university, Israel.
+
+## Code Design:
+We use with the node.js technology for the server app, and the client web-page with html&css. all this by the MVC architecture. 
+
+## REST-API:
+The sever is bind to the port - 8080 and the ip is your localhost if you download this project to your own computer.
+There are a few http functions that the API supports and  will extend for each one of them which type is it and what it is url.
+1. GET - '/' - by the '/' the client will get the web-page that he can connect to the server from there - \
+The client shold upload two files - train and test file, and also to choose  which algoritem he want to test the flight.\
+There are two possible algoritems -\
+A. Line Regression.\
+B. MinCircle.\
+After uploading the files - the server return him which features have irregular activity and when the devion started and when its ended.
+
+2.GET- /api/amodel - given a id of the client the server will return is status by a json, the given id should by at the query with the name 'model_type'.
+
+3.GET- /api/amodels - the server will return all the clients that where connect to him by a json.
+
+4.POST -/api/detect -the client should upload two files - if just one of them will upload the server will return an error,\
+the names of the files should be at the body request by the name - train and test. And the server will return the irregular activity by json to the client.
+
+5.DELETE - /api/model - given an id of the client - the server will erase him from his list of models.
+
+## Structure project:
+There are a few folders:
+
+1.In the view folder you have the the view control for the web-page.
+
+2.In the Model folder - there is the model server that do all the work behind.
+
+3.In node_models folder there are all the liberys you need.
+
+4.In the files folder - there are the files you may upload to the server - train and test file.
+
+# Installation for running the App:
+1. Use git clone https://github.com/avivdimri/webApp.git to downloads the project.
+2. Make sure you already has a node on your computer - if not you may downloads from here for free - https://nodejs.org/en/download/.you may use any operation systeam you want.
+3. Use node contorller.js to run the server.
+4. Try to connect the server from any browser or progream language to connect the server by port - 8080,you may change it if you want.
+5. You may use one or any of the function we declare at the REST-API above.
+
+## UML:
+We can see in the UML below of the App which based on the MVC architecture. We bulided the View and then put them in the Main Window. Every View has View Model of its own, which is as the model for the View. For the View, it is an abstraction of the Model.It passes commands from the view to the model.
+
+The Views Model Converts model information into view information. The Model and every View Model Implement INotifyPropertyChanged interface and notify about changes for the observers like a View Model or View. The views Model get notifications from the model by adding delegates to its PropertyChangedevent. Eventually we used Data binding within the UI.
+
+If a View want to make change in the model we used with functions.
+
+Also, the Model has a client which resposinble about the connection with the Flight Gear Simulator. In the left side we can see that the View of the Graph has a member of dinamic dll which responsible on the connect of the dll View to the Main Window. The dll can be any algorithm whichimplement the interrface by name IntrfaceDll which contain the 3 function Create,Update,Time(its cut in the picture)
+
+
+![UML](https://user-images.githubusercontent.com/80414213/120098860-f062b300-c140-11eb-87eb-0e46f113292d.png)
+
+
+
+
+
